bfe0d6ca698245cc8c9e9ca64e42a5d9dc2ffec1
Svmpankaj <svmpankaj@gmail.com>
2023-10-28 20:05:15 +0530

    mongodb is connected

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
 
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const expressLayout = require('express-ejs-layouts');
+
+const connectDB = require('./server/config/db');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
+// Connect to DB
+connectDB();
+
 app.use(express.static('public'))
 
 // Templating Engine
-app.use(expressLayouts);
+app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
