ea7a68b1f4c9734b8ef1df00af13d0288cc2257b
Svmpankaj <svmpankaj@gmail.com>
2023-11-09 18:17:32 +0530

    admin page is created and login

---
+++ server/routes/admin.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+const User = require('../models/User');
+
+const adminLayout = '../views/layouts/admin';
+/** 
+ * GET /
+ * Admin - Login Page
+*/
+
+router.get('/admin', async (req, res) => {
+
+    try {
+        const locals = {
+            title: "Admin",
+            description: "Simple Blog create with NodeJs, Express & MongoDB."
+        }
+
+        res.render('admin/index', { locals });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+/** 
+ * POST /
+ * Admin - Check Login
+*/
+
+router.post('/admin', async (req, res) => {
+
+    try {
+
+        const { username, password } = req.body;
+        console.log(req.body);
+        res.redirect('/admin');
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+
+module.exports = router;
